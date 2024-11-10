6eaf5b8ab5debc718192b806cc700bcc7e48e4c4
Adriano Junior <adrianojunior.dev97@gmail.com>
2024-11-10 03:48:54 -0300

    feat: implement db connection initialize and add new models

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,38 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URl = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as { mongoose?: MongooseConnection })
+  .mongoose || { conn: null, promise: null };
+
+if (!cached) {
+  cached = (global as { mongoose?: MongooseConnection }).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!MONGODB_URl) {
+    throw new Error("Missing MONGODB_URL");
+  }
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URl, {
+      dbName: "image-processor-ai-sass",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
