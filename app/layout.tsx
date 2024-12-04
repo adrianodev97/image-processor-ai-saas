354e2eecedc8ca7d3f54b815a6dbc4d21c13184f
Adriano Junior <adrianojunior.dev97@gmail.com>
2024-12-04 00:44:36 -0300

    config: change html lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         },
       }}
     >
-      <html lang="en">
+      <html lang="pt-BR">
         <body className={cn("font-IBMPlex antialised", IBMPlex.variable)}>
           {children}
         </body>
